7cc684c1f36e64074fdb64583dad49031928eacf
MrcoSchrnr <MarcoSchreiner@web.de>
2024-03-13 11:43:33 +0100

    added structure of library

---
+++ gb_DFR0523_DigitalPeristalticPump.h
@@ -0,0 +1,12 @@
+#ifndef gb_DFR0523_DigitalPeristalticPump.h
+#define gb_DFR0523_DigitalPeristalticPump.h
+
+#include <Arduino.h>
+
+class gb_DFR0523_DigitalPeristalticPump
+{
+private:
+public:
+};
+
+#endif
